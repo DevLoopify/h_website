e9b0ad5e509bb61ed8d22c3309b354ad7deec39b
DevLoopify <yodamaddog@gmail.com>
2025-09-24 17:46:23 +0200

    add out folder

---
+++ out/_next/static/HMhAF4hqSlCKeU8bxj67F/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/38f71919ce425769.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
